44b893f7887c7abfd328ea9ae2d080241e10ab77
Mateo-mh <byronmh8@gmail.com>
2023-01-06 01:36:34 -0500

    Creando funciones para poder ingresar valores

--- app/mail.py
+++ app/mail.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, flash
+    Blueprint, render_template, request, flash, url_for, redirect
 )
 
 from app.db import get_db
@@ -33,9 +33,16 @@ def create():
             errors.append('Contenido es obligatorio')
 
         if len(errors) == 0:
-            pass
+            db, c = get_db()
+            c.execute("INSERT INTO email (email, subject, content) VALUES (%s, %s, %s)", (email, subject, content))
+            db.commit()
+
+            return redirect(url_for('mail.index'))
         else:
             for error in errors:
                 flash(error)
 
     return render_template('mails/create.html')
+
+def send(to, subject, content):
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
